ebcfee663a9465750dc7253061606efad6d3d8b0
VMJauregui <21jaureguiv@ofcs.net>
2019-02-27 19:26:37 -0500

    Completed Movie Program. Continued with Challenges.

--- Week03/MovieList/MovieList/MainWindow.xaml.cs
+++ Week03/MovieList/MovieList/MainWindow.xaml.cs
@@ -33,12 +33,30 @@ namespace MovieList
 
         private void buttonAddMovie_Click(object sender, RoutedEventArgs e)
         {
+            string name = textBoxTitle.Text;
+            textBoxTitle.Clear();
+            int year = Convert.ToInt32(textBoxReleaseYear.Text);
+            textBoxReleaseYear.Clear();
+            string director = textBoxDirector.Text;
+            textBoxDirector.Clear();
+            int length = Convert.ToInt32(textBoxLength.Text);
+            textBoxLength.Clear();
+            string genre = textBoxgenre.Text;
+            textBoxgenre.Clear();
 
+            Movie newMovie = new Movie(name, year, director, length, genre);
+
+            MovieList.Add(newMovie);
+            
         }
 
         private void buttonShowAll_Click(object sender, RoutedEventArgs e)
         {
-
+            //loop to go through the list
+            foreach (Movie movie in MovieList)
+            {
+                movie.ShowMovie();
+            }
         }
     }
 }
--- Week03/MovieList/MovieList/Movie.cs
+++ Week03/MovieList/MovieList/Movie.cs
@@ -5,6 +5,25 @@ namespace MovieList
 {
     public class Movie
     {
+        private string movieName;
+        private int releaseYear;
+        private string director;
+        private int length;
+        private string genre;
 
+        public Movie (string Name, int ReleaseYear, string Director, int Length, string Genre)
+        {
+            //this. is not needed.
+            this.movieName = Name;
+            this.releaseYear = ReleaseYear;
+            this.director = Director;
+            this.length = Length;
+            this.genre = Genre;
+        }
+
+        public void ShowMovie()
+        {
+            MessageBox.Show(movieName + Environment.NewLine + releaseYear + Environment.NewLine + director + Environment.NewLine + length + Environment.NewLine + genre);
+        }
     }
 }
