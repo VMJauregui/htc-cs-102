b17dfad70f14ed5cf3f33224723149d6706b5509
VMJauregui <21jaureguiv@ofcs.net>
2019-04-10 19:29:48 -0400

    Followed the clicker.

---
+++ Week10/WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EllipseGeometry myEllipseGeometry;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            myEllipseGeometry = new EllipseGeometry();
+            myEllipseGeometry.Center = new Point(200, 200);
+            myEllipseGeometry.RadiusX = 20;
+            myEllipseGeometry.RadiusY = 20;
+
+            Path myPath = new Path();
+            myPath.Fill = Brushes.SkyBlue;
+            myPath.Data = myEllipseGeometry;
+
+            myCanvas.Children.Add(myPath);
+
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            PointAnimation myPointAnimation = new PointAnimation(e.GetPosition(this), TimeSpan.FromSeconds(1));
+            myEllipseGeometry.BeginAnimation(EllipseGeometry.CenterProperty, myPointAnimation);
+            myPointAnimation.EasingFunction = new CircleEase();
+        }
+    }
+}
